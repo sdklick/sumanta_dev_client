dd4b2dcdf638f1530f58bda900f93c6c1364e43d
sdklick <sumantad827@gmail.com>
2024-12-11 09:53:33 +0530

    add new tech on banner

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -64,7 +64,12 @@ const Navbar = () => {
           <div class="col-md-6 col-12 column">
             <h1 class="mb-2">Hello, I'm</h1>
             <h2 class="display-4 fw-bold">Sumanta Das</h2>
+            <hr/>
             <h3 class="text-success">MERN Developer</h3>
+            <hr/>
+            <h3 class="text-success">Cloud Enthusiast</h3>
+            <hr/>
+            <h3 class="text-success">Ai ML Enthusiast</h3>
             <p class="mt-3">
               I'm a software engineer based in Kolkata. I'm committed to
               continually growing and innovating within the ever-evolving field
