3d744f53867074d502675a1a7726f7bb06eb5130
sdklick <sumantad827@gmail.com>
2024-12-11 09:42:13 +0530

    some file delete

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,11 @@ import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.js";
 import "./App.css"
 import Navbar from './components/Navbar.jsx';
-import Header from './components/Header.jsx';
-import About from './components/About.jsx';
 
 const App = () => {
   return (
     <>
     <Navbar/>
-    {/* <Header/>
-    <About/> */}
     </>
   )
 }
