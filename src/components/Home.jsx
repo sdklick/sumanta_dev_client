270f79112e4fb8db6860d8293316efbe84435c55
sdklick <sumantad827@gmail.com>
2024-12-19 16:27:04 +0530

    Home name change

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,31 +1,34 @@
 import React from "react";
 import "../styles/navbar.css";
+import { NavLink } from "react-router-dom";
 
 const Home = () => {
   return (
     <>
-      <div class="container-fluid  px-5 pt-5 bg-light banner-background d-grid">
-        <div class="row">
-          <div class="col-md-6 col-12 column">
-            <h1 class="mb-2">Hello, I'm</h1>
-            <h2 class="display-4 fw-bold">Sumanta Das</h2>
+      <div className="container-fluid  px-5 pt-5 bg-light banner-background d-grid">
+        <div className="row">
+          <div className="col-md-6 col-12 column">
+            <h1 className="mb-2">Hello, I'm</h1>
+            <h2 className="display-4 fw-bold">Sumanta Das</h2>
             <hr />
             <h3 style={{ color: "orangered" }}>MERN Developer</h3>
             <hr />
             <h3 style={{ color: "orangered" }}>Cloud Enthusiast</h3>
             <hr />
             <h3 style={{ color: "orangered" }}>Ai ML Enthusiast</h3>
-            <p class="mt-3">
+            <p className="mt-3">
               I'm a software engineer based in Kolkata. I'm committed to
               continually growing and innovating within the ever-evolving field
               of software development.
             </p>
-            <a href="#" class="btn btn-success border rounded-pill">
-              About Me
-            </a>
+            <button className="btn btn-success border rounded-pill">
+              <NavLink to="/aboutme" className="nav-link">
+                Aboutme
+              </NavLink>
+            </button>
           </div>
 
-          <div class="col-md-6 col-12 profile-photo"></div>
+          <div className="col-md-6 col-12 profile-photo"></div>
         </div>
       </div>
     </>
