8783fb476036bb5c7e57c92618460694043a3741
sdklick <sumantad827@gmail.com>
2025-03-07 21:59:51 +0530

    improve footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,7 +9,52 @@ import { FaWhatsapp } from "react-icons/fa";
 const Footer = () => {
   return (
     <>
-      <footer>
+      <div className="container">
+        <div className="row d-flex flex-lg-row flex-md-column flex-sm-column flex-column column-gap-3">
+          <div className="col text-light text-center d-flex my-3 justify-content-lg-start justify-content-md-center justify-content-sm-center justify-content-center overflow-auto">
+            &copy; 2024 All Right Reserved | Developed By Sumanta Das
+          </div>
+          <div className="col d-flex gap-5 justify-content-lg-end  justify-content-md-center justify-content-sm-center justify-content-center my-3 overflow-auto">
+            <a
+              href="https://github.com/sdklick"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaGithub size={32} color="white" />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/sumantad827/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaLinkedinIn size={32} color="#008ad8" />
+            </a>
+            <a
+              href="mailto:sumantad827@gmail.com?subject=Hello&body=I%20would%20like%20to%20connect%20with%20you!"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <BiLogoGmail size={32} color="red" />
+            </a>
+
+            <a
+              href="mailto:sumantad827@outlook.com?subject=Hello&body=I%20would%20like%20to%20connect%20with%20you!"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <PiMicrosoftOutlookLogo size={32} color="#008ad8" />
+            </a>
+            <a
+              href="https://wa.me/8637073059?text=Hello%20there!"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaWhatsapp size={32} color="green" />
+            </a>
+          </div>
+        </div>
+      </div>
+      {/* <footer>
         <div className="footer2">
           <div className="copyright">
             <p style={{ width: "85%", textAlign: "center" }}>
@@ -54,9 +99,12 @@ const Footer = () => {
             >
               <FaWhatsapp size={32} color="green" />
             </a>
+            
           </div>
+          
         </div>
-      </footer>
+       
+      </footer> */}
     </>
   );
 };
