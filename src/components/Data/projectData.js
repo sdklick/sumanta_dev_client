5c3090a581ee13326f42ad1e9a0aa1ec25491a90
sdklick <sumantad827@gmail.com>
2025-03-12 13:09:43 +0530

    Project Data Add

---
+++ src/components/Data/projectData.js
@@ -0,0 +1,25 @@
+import sumantadev from "../../assets/sumantadev.png";
+import cvapp from "../../assets/cvapp.png";
+import examapp from "../../assets/Examapp.png";
+const projectData = [
+  {
+    projectname: "My Portfolio",
+    projectcode: "https://github.com/sdklick/sumanta_dev_client",
+    projectlink: "https://sumantadev.netlify.app/",
+    projectimg: sumantadev,
+  },
+  {
+    projectname: "Online Examination",
+    projectcode: "https://github.com/sdklick/Exam_client",
+    projectlink: "https://sumanta-learn-app.netlify.app/",
+    projectimg: examapp,
+  },
+  {
+    projectname: "Cv Builder",
+    projectcode: "https://github.com/sdklick/CV_client",
+    projectlink: "https://sumanta-cv-app.netlify.app/",
+    projectimg: cvapp,
+  },
+];
+
+export default projectData;
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,88 +1,45 @@
 import React from "react";
-import cvapp from "../assets/cvapp.png";
-import sumantadev from "../assets/sumantadev.png";
-import examapp from "../assets/Examapp.png";
-
+import projectData from "./Data/projectData";
 const Projects = () => {
   return (
     <>
       <div className="container-fluid my-5">
         <div className="row d-flex justify-content-center gap-5">
-          <div className="col-12 col-sm-5 col-md-5 col-lg-3">
-            <div className="card">
-              <img src={sumantadev} className="card-img-top" alt="..." />
-              <div className="card-body">
-                <h5 className="card-title text-center">My PortFolio</h5>
+          {projectData.map((val, idx) => {
+            return (
+              <div key={idx} className="col-12 col-sm-5 col-md-5 col-lg-3">
+                <div className="card">
+                  <img
+                    src={val.projectimg}
+                    className="card-img-top"
+                    alt="..."
+                  />
+                  <div className="card-body">
+                    <h5 className="card-title text-center">
+                      {val.projectname}
+                    </h5>
 
-                <div className="d-flex justify-content-between">
-                  <a
-                    href="https://sumantadev.netlify.app/"
-                    target="_blank"
-                    className="btn btn-primary"
-                  >
-                    <i className="fas fa-eye"></i>
-                  </a>
-                  <a
-                    href="https://github.com/sdklick/sumanta_dev_client"
-                    target="_blank"
-                    className="btn btn-primary"
-                  >
-                    <i className="fas fa-code"></i>
-                  </a>
+                    <div className="d-flex justify-content-between">
+                      <a
+                        href={val.projectlink}
+                        target="_blank"
+                        className="btn btn-primary"
+                      >
+                        <i className="fas fa-eye"></i>
+                      </a>
+                      <a
+                        href={val.projectcode}
+                        target="_blank"
+                        className="btn btn-primary"
+                      >
+                        <i className="fas fa-code"></i>
+                      </a>
+                    </div>
+                  </div>
                 </div>
               </div>
-            </div>
-          </div>
-          <div className="col-12 col-sm-5 col-md-5 col-lg-3">
-            <div className="card">
-              <img src={examapp} className="card-img-top" alt="..." />
-              <div className="card-body">
-                <h5 className="card-title text-center">Online Examination</h5>
-
-                <div className="d-flex justify-content-between">
-                  <a
-                    href="https://sumanta-learn-app.netlify.app/exampage"
-                    target="_blank"
-                    className="btn btn-primary"
-                  >
-                    <i className="fas fa-eye"></i>
-                  </a>
-                  <a
-                    href="https://github.com/sdklick/Exam_client"
-                    target="_blank"
-                    className="btn btn-primary"
-                  >
-                    <i className="fas fa-code"></i>
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col-12 col-sm-10 col-md-10 col-lg-3">
-            <div className="card">
-              <img src={cvapp} className="card-img-top" alt="..." />
-              <div className="card-body">
-                <h5 className="card-title text-center">Cv Builder</h5>
-
-                <div className="d-flex justify-content-between">
-                  <a
-                    href="https://sumanta-cv-app.netlify.app/"
-                    target="_blank"
-                    className="btn btn-primary"
-                  >
-                    <i className="fas fa-eye"></i>
-                  </a>
-                  <a
-                    href="https://github.com/sdklick/CV_client"
-                    target="_blank"
-                    className="btn btn-primary"
-                  >
-                    <i className="fas fa-code"></i>
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
+            );
+          })}
         </div>
       </div>
     </>
